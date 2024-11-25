f30cba52339d9e8f8417fc435ca037be6c8d554b
Ramkishore045 <ramkishore0268@gmail.com>
2024-11-25 19:20:40 +0530

    Resolve_conflicts

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Home from './Home';
 import Contact from './Contact/ContactUs';
 import LoginPage from './Login/LoginPage';
 import Register from './Rigester/Register';
+import SalesReport from './SalesReport/SalesReport';
 import SupplierManagement from './SupplierManagement/SupplierManagement';
 
 
@@ -41,6 +42,7 @@ function App() {
     { path: "/contact-us", element: <Contact /> },
     { path: "/login-page", element: <LoginPage /> },
     { path: "/signup", element: <Register /> },
+    {path:"/sales-report",element: <SalesReport />},
     { path: "supplier-management", element: <SupplierManagement /> },
     
 
